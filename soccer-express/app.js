dab37c895f8bd970f5731ea10fb5311cf604401f
MinhyeSim <smine0032@gmail.com>
2022-04-02 22:16:56 +0900

    first commit

---
+++ soccer-express/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+var cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const { port, MONGO_URI } = process.env;
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 
+}
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+app.listen(port, () => {
+    console.log({"현재 시간 : ":new Date().toLocaleString()})
+  })
+app.get('/', (req, res) => {
+  res.json({"현재 시간 : ":new Date().toLocaleString()})
+})
+app.get('/api/now', cors(corsOptions),(req, res) => {
+  res.json({"now":new Date().toLocaleString()})
+})
+app.get('/', (req, res) => {
+  res.json({"현재 시간 : ":new Date().toLocaleString()})
+})
+app.get('/api/now', cors(corsOptions),(req, res) => {
+  res.json({"now":new Date().toLocaleString()})
+})
+
