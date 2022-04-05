29fe2f2922c9c2738792425531077b0a7809e44d
MinhyeSim <smine0032@gmail.com>
2022-04-05 10:20:28 +0900

    2022-04-05 10:19 express middleware router

--- soccer-express/app.js
+++ soccer-express/app.js
@@ -41,27 +41,64 @@ app.post("/api/board/write",(req, res) => {
   console.log(`subject 값 : ${subject}`)
   res.json(req.body)
 })
-app.post("/api/bmi/write",(req, res) => {
+function computeBMI(payload){
+  const {name, height, weight} = payload
+      //Obtain user inputs
+      let _height=Number(height);
+      let _weight=Number(weight);
+
+      //Perform calculation
+      let bmi = _weight/Math.pow(_height,2);
+
+      let output = Math.round(bmi*100)/100;
+      var result = {name, height, weight}
+      console.log(`계산중인 값들 : ${JSON.stringify(result)}`)
+      if (output<18.5)
+        result.bmi = "저체중";
+      if (output>=18.5 && output<=25)
+        result.bmi = "정상";
+      if (output>=25 && output<=30)
+        result.bmi = "과체중";
+      if (output>30)
+        result.bmi = "경도비만";
+        console.log(`계산끝난 값들 : ${JSON.stringify(result)}`)
+      return result
+}
+app.post("/api/basic/bmi", (req, res)=>{
   const {name, height, weight} = req.body
-  console.log(`넘어온 JSON 값 : ${JSON.stringify(res.body)}`)
-  console.log(`name값 : ${name}`)
-  console.log(`height값 : ${height}`)
-  console.log(`weight값 : ${weight}`)
+  console.log(`넘어온 JSON 값 : ${JSON.stringify(req.body)}`)
+  console.log(`이름 : ${name}`)
+  console.log(`키 : ${height}`)
+  console.log(`몸무게 : ${weight}`)
+  const json = computeBMI(name, height, weight)
+  console.log(`계산된 JSON 값 : ${JSON.stringify(json)}`)
   res.json(json)
 })
-function computeBMI({name, weight, height}){
-  let _height = Number(height)
-  let _weight = Number(weight)
-  let bmi = _weight/Math.pow(_height,2)
-  let output = Math.round(bmi*100)/100
-  const result = {name}
-  if (output<18.5)
-    result.bmi = "Underweight"
-  if (output>=18.5)
-    result.bmi = "Normal"
-  if (output>25)
-    result.bmi = "Obese"
-  if (output>30)
-    result.bmi = "Overweight"
-  return result
+function computeCALC(num1, opcode, num2){
+  console.log(' #### 진입  ### ')
+      //Obtain user inputs
+      var result = {num1, opcode, num2}
+      console.log(`계산중인 값들 : ${JSON.stringify(result)}`)
+      if (opcode == '+')
+        result.calc = (num1)+(num2);
+      if (opcode == '-')
+        result.calc = (num1)-(num2);
+      if (opcode == '*')
+        result.calc = (num1)*(num2);
+      if (opcode == '/')
+        result.calc = (num1)/(num2);
+      if (opcode == '%')
+        result.calc = (num1)%(num2);    
+        console.log(`계산끝난 값들 : ${JSON.stringify(result)}`)
+      return result
 }
+app.post("/api/basic/calc", (req, res)=>{
+  const {num1, opcode, num2} = req.body
+  console.log(`넘어온 JSON 값 : ${JSON.stringify(req.body)}`)
+  console.log(`숫자1 : ${num1}`)
+  console.log(`연산자 : ${opcode}`)
+  console.log(`숫자2 : ${num2}`)
+  const json = computeCALC(num1, opcode, num2)
+  console.log(`계산된 JSON 값 : ${JSON.stringify(json)}`)
+  res.json(json)
+})
---
+++ soccer-express/app/routes/users.routesjs
@@ -0,0 +1,18 @@
+/**
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+*/
+module.exports = function(app){
+  app.use((req, res, next)=>{
+    res.header(
+    )
+  });
+  // next();
+  app.get("", ()=>{})
+  app.post("", ()=>{})
+  app.update("", ()=>{})
+  app.delete("", ()=>{})
+};
